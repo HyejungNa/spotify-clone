89e63de4ea0f9626e18439feaf7c57871ecd2a66
Hyejung Na <hyejung.na01@gmail.com>
2025-06-03 22:48:20 +1000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router";
+import AppLayout from "./layout/AppLayout";
+import HomePage from "./pages/HomePage/HomePage";
+import SearchPage from "./SearchPage/SearchPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />}></Route>
+        <Route path="search" element={<SearchPage />}></Route>
+        {/* <Route
+          path="search/:keyword"
+          element={<SearchWithKeywordPage />}
+        ></Route>
+        <Route path="playlist/:id" element={<PlaylistDetailPage />}></Route> */}
+        {/* mobile version playlist page */}
+        {/* <Route path="playlist" element={<PlaylistPage />}></Route> */}
+      </Route>
+    </Routes>
   );
 }
 
