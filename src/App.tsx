de77c0a9154660e1c9a90903c6be2210ca0adff5
Hyejung Na <hyejung.na01@gmail.com>
2025-06-04 01:13:07 +1000

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router";
-import AppLayout from "./layout/AppLayout";
-import HomePage from "./pages/HomePage/HomePage";
-import SearchPage from "./SearchPage/SearchPage";
+const AppLayout = React.lazy(() => import("./layout/AppLayout"));
+const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
+const SearchPage = React.lazy(() => import("./pages/SearchPage/SearchPage"));
+const SearchWithKeywordPage = React.lazy(
+  () => import("./pages/SearchWithKeywordPage/SearchWithKeywordPage")
+);
+const PlaylistDetailPage = React.lazy(
+  () => import("./pages/PlaylistDetailPage/PlaylistDetailPage")
+);
+const PlaylistPage = React.lazy(
+  () => import("./pages/PlaylistPage/PlaylistPage")
+);
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<HomePage />}></Route>
-        <Route path="search" element={<SearchPage />}></Route>
-        {/* <Route
-          path="search/:keyword"
-          element={<SearchWithKeywordPage />}
-        ></Route>
-        <Route path="playlist/:id" element={<PlaylistDetailPage />}></Route> */}
-        {/* mobile version playlist page */}
-        {/* <Route path="playlist" element={<PlaylistPage />}></Route> */}
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>loading...</div>}>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<HomePage />}></Route>
+          <Route path="search" element={<SearchPage />}></Route>
+          <Route
+            path="search/:keyword"
+            element={<SearchWithKeywordPage />}
+          ></Route>
+          <Route path="playlist/:id" element={<PlaylistDetailPage />}></Route>
+          <Route path="playlist" element={<PlaylistPage />}></Route>
+          <Route path="*" element={<div>Page Not Found</div>}></Route>
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
